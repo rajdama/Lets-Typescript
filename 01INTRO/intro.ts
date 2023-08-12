769ae723d3965f2b1ccd0f8ffea16b7bae34edc9
unknown <Raj Dama>
2023-08-12 16:33:15 +0530

    more types

--- 01INTRO/intro.ts
+++ 01INTRO/intro.ts
@@ -1,2 +1,4 @@
 let user = {name:"raj", age:"21"}
- console.log(user.height) //Error detected in typescript
+console.log(user.height) //Error detected in typescript
+
+export {}
--- 01INTRO/variableMe.ts
+++ 01INTRO/variableMe.ts
@@ -1,3 +1,10 @@
+//string
 let greetings: string = "Hello Raj" //Defining variable type
+
+//number
+let userId: number = 3344
+
+//boolean
+let isLoggedIn: boolean = false
 
 console.log(greetings)

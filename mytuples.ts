ca673f00b56f54e45ee4d13a576d4257946c0e0c
Raj <rajdama1729@gmail.com>
2023-08-22 12:51:23 +0530

    tuples

---
+++ mytuples.ts
@@ -0,0 +1,6 @@
+let tUser: [string, number, boolean];
+tUser = ["hc", 131, true];
+let rgb: [number, number, number] = [255, 123, 112];
+
+type tuser = [number, string];
+const newUser: tuser = [112, "example@google.com"];
